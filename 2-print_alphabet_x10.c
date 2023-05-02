0551b3e3ffa7980ac85994b098b39269a700c03d
toxicfrank <ngugifrancis930@gmail.com>
2023-05-02 21:43:06 +0100

    practice

---
+++ 2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * function that prints 10 times the alphabet, in lowercase,
+ * followed by a new line.
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	for ( a= 1; a <= 10; a++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
