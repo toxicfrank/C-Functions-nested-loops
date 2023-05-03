0996ee98affec2af5e3a78eed0a57a3450f7a590
toxicfrank <ngugifrancis930@gmail.com>
2023-05-03 12:44:36 +0100

    practice

---
+++ 9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints the 9 times table,
+ * starting with 0.
+ * @void: function parameter
+ * Return: inetegers
+ */
+void times_table(void)
+{
+	
+}
